c3a117bc2a6a1b39e8ef58a387bc09e66adb6d8b
Marcelle <Marcelle>
2026-08-25 11:53:38 -0300

    roas login e av1 dws

--- src/server.js
+++ src/server.js
@@ -5,4 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Banco de dados conectado !`);
+ 
 });
